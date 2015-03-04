71c6817ad583f494d0ae603cb83cfb6d3427d7c8
Benjamin <benjak@math.su.se>
2015-03-04 10:06:14 +0100

    Removed unneeded functions

--- R/mbss_likelihood_ratios.R
+++ R/mbss_likelihood_ratios.R
@@ -4,34 +4,25 @@
 
 
 
-llr_sums_over_stream <- function() {
-  DT[time < maximum_duration,
-     , 
-     by = .(region, time, severity)]
-}
-
-
-#' Sums the location-independent part of the LLR for all regions.
+#' Computes the full log-likelihood ratios.
 #' 
+#' Computes the full log-likelihood ratios
+#' LLR_S^{k,l}(W) = log(\eqn{P(D | H_1(S,E_k), W, \theta_l^k) / P(D|H_0)})
+#' for all values 1 <= W <= W_max. 
 #' 
-llr_region_sum <- function() {
-  dt[time < maximum_duration, 
-     sum(likelihood) / number_of_severities, 
-     by = .(region, event, time)]  
-}
-
-
-#' Computes log(\eqn{P(D | H_1(S,E_k), W, \theta_l^k) / P(D|H_0)})
-#' for all values 1 <= W <= W_max
+#' @param lq_table A \code{data.table} 
 full_llr <- function(lq_table) {
   lq_table[, .(time = time, llr = cumsum(lq)), 
            by = .(severity, event, region)]
 }
 
+#' Computes log-likelihood contribution for each time step.
+#' 
 #' Adds the region independent and dependent LLR parts together
 #' over the data stream, then sums over the stream.
 #' 
-#' Both tables must have the following columns as the first 5 keys:
+#' @section Important:
+#' Both tables must have the following columns as the first 4 keys:
 #' \code{c("stream", "time", "severity", "event")}.
 #' 
 #' @param region_table A \code{data.table} with
@@ -51,7 +42,6 @@ sum_over_streams <- function(region_table,
 #'        \code{lg} contains that part of the (full) log-likelihood
 #'        ratio which depends on the locations contained in spatial region S.
 sum_locations_in_region <- function(lg_table) {
-  # sum_{i \in S} log g_{W-1}(i,m|x_{m,l}^k)
   lg_table[, .(slg = sum(lg)), 
            keyby = c("stream", "time", "severity", "event", "region")]
 } 
