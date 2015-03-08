e6a2d217bcaca517700ab5fa32a93ce61e0b7244
Benjamin <benjak@math.su.se>
2015-03-08 20:21:45 +0100

    Normalize all the line endings

---
+++ R/mbss_posterior_probabilities.R
@@ -0,0 +1,14 @@
+
+# Warning: modifies argument by adding a column
+spacetime_logposterior <- function(spacetime_llh_table,
+                                   event_occurrence_prior_logprobs,
+                                   number_of_regions,
+                                   event_duration_prior_logprobs,
+                                   data_logprob) {
+  
+}
+
+
+data_logprob <- function() {
+  
+}
---
+++ tests/testthat/test_mbss_posterior_probabilities.R
@@ -0,0 +1,5 @@
+context("MBSS posterior probabilities")
+
+test_that("spacetime_logposterior: calculated correctly", {
+  
+})
