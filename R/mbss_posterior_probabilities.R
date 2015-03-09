ed7f9ad210bb03b2ebbadbc687e0bd83a8d7b3bf
Benjamin <benjak@math.su.se>
2015-03-09 09:33:35 +0100

    Spacetime event log-probability implemented.

--- R/mbss_posterior_probabilities.R
+++ R/mbss_posterior_probabilities.R
@@ -1,14 +1,26 @@
 
 # Warning: modifies argument by adding a column
-spacetime_logposterior <- function(spacetime_llh_table,
-                                   event_occurrence_prior_logprobs,
-                                   number_of_regions,
-                                   event_duration_prior_logprobs,
+
+#' Calculates the posterior probability of an event in a space-time window.
+#' 
+#' Calculates the posterior probability of an event of a given type \eqn{E_k}
+#' for a given spatial region \eqn{S} and a given event duration \eqn{W},
+#' for all event types, spatial regions and event durations up to \eqn{W_max}.
+#' This function \strong{modifies} the table passed as the first argument.
+#' 
+#' @param spacetime_llhs A \code{data.table}
+#' @param event_logpriors A vector of the logarithms of the prior 
+#'        probabilities of an event, ordered from event type 1 to K.
+#' @param n_regions The number of regions.
+#' @param max_duration The number of time periods of the longest
+#'        event duration considered.
+#' @param data_logprob The logarithm of the marginal probability of the data.
+spacetime_logposterior <- function(spacetime_llh,
+                                   event_logpriors,
+                                   n_regions,
+                                   max_duration,
                                    data_logprob) {
-  
+  spacetime_llh[, posterior_logprob := llh + event_logpriors[event]
+                - log(n_regions) - log(max_duration) - data_logprob]
 }
 
-
-data_logprob <- function() {
-  
-}
--- tests/testthat/test_mbss_posterior_probabilities.R
+++ tests/testthat/test_mbss_posterior_probabilities.R
@@ -1,5 +1,13 @@
 context("MBSS posterior probabilities")
 
 test_that("spacetime_logposterior: calculated correctly", {
+  st_lps <- data.table(region = rep(1:3, each = 4),
+                       event = rep(1:2, 3, each = 2),
+                       time = rep(0:1, 6), 
+                       llh = rep(c(0.5, 2), 3, each = 2))
+  st_lps[, llh := llh + 1:12]
+  setkeyv(st_lps, c("region", "event", "time"))
   
+  spacetime_logposterior(st_lps, c(0.5, -1), exp(1), exp(2), 3)
+  expect_equal(st_lps[, posterior_logprob], 1:12 - 5)
 })
