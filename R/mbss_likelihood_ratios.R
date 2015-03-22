24dbe5f74582a4a4d330baecb943369aa682cf51
Benjamin <benjak@math.su.se>
2015-03-22 23:20:21 +0100

    Rethinking way to calculate likelihood ratios. Need to change likelihood functions to accomodate.

--- R/mbss_likelihood_ratios.R
+++ R/mbss_likelihood_ratios.R
@@ -1,6 +1,47 @@
 
 # Note: we only calculate (log) likelihood ratios for time steps 0 <= t < W_max
 
+# Input is a table with the log-likelihood ratio given the event effect, 
+# and the log-probability of that effect,
+# for each event, location, time, and stream.
+# The function marginalizes over the effects.
+# Calculates the log-likelihood ratios for each event, time, location,
+# and stream.
+event_llr <- function(llrs_given_effects) {
+  llrs_given_effects[, .(llr = logsumexp(llr + effect_logprob)),
+                     keyby = .(location, event, time, stream)]
+}
+
+
+#' Sums individual LLRs over all locations in each region.
+#' 
+#' For a given data stream \eqn{m}, time step \eqn{t}, region \eqn{S},
+#' and event \eqn{E_k}, 
+#' sums the log-likelihood ratios of the counts over the locations in \eqn{S}.
+#' 
+#' @param llr_table A \code{data.table} containing columns \code{location}
+#'        and column \code{llr}, and the following columns as keys in
+#'        the given order: \code{region}, \code{event}, \code{time}, 
+#'        \code{severity}, \code{stream}.
+#' @return A \code{data.table} with the same key columns as the input 
+#'         \code{data.table}, with column \code{llr} containing the sum
+#'         of the column \code{llr} in the input \code{data.table}
+#'         over all locations in each region.
+spacetime_llr <- function(event_llrs) {
+  event_llrs[, .(llr = sum(llr)), by = .(region, event, time)][,
+    .(time = time, llr = cumsum(llr)), by = .(region, event)]
+} 
+
+
+# eventtime_logprobs = matrix, rows representing event duration,
+# columns representing event types,
+# elements are log-probabilities of that duration for that event
+spatial_llr <- function(st_llrs, eventtime_logprobs) {
+  st_llrs[, .(llr = logsumexp(llr + eventtime_logprobs[time + 1, event])), 
+          by = .(region, event)]
+}
+
+
 #' Computes the full log-likelihood ratios.
 #' 
 #' Computes the full log-likelihood ratios; the log-likelihood ratio
--- tests/testthat/test_mbss_likelihood_ratios.R
+++ tests/testthat/test_mbss_likelihood_ratios.R
@@ -1,5 +1,32 @@
 context("MBSS likelihood ratio functions")
 
+
+test_that("spacetime_llr: sums correctly", {
+  y <- data.table(location = c(rep(1:2, each = 6), rep(1:2, 6)),
+                  region = c(rep(1:2, each = 6), rep(3, 12)),
+                  event = rep(1, 24),
+                  time = c(rep(0:2, 2, each = 2), rep(0:2, each = 4)),
+                  stream = c(rep(1:2, 6), rep(1:2, 3, each = 2)),
+                  llr = 1:24)
+  setkeyv(y, c("region", "event", "time", "stream", "location"))
+  
+  stllr <- spacetime_llr(y)
+  expect_equal(stllr[, llr], c(3, 10, 21, 15, 34, 57, 58, 132, 222))
+})
+
+test_that("event_llr: calculates correctly", {
+  llrs <- data.table(location = rep(1:2, each = 8),
+                     event = rep(1:2, 4, each = 2),
+                     time = rep(0:1, 2, each = 4),
+                     stream = rep(1L, 16),
+                     effect_logprob = 16:1,
+                     llr = 1:16)
+  setkeyv(llrs, c("location", "time", "event"))
+  evllr <- event_llr(llrs)
+  expect_equal(evllr[, llr], c(rep(17 + log(2), 8)))
+})
+
+
 test_that("full_llr: cumsum over time correct when no missing values", {
   llrs <- data.table(region = rep(1:3, each = 3),
                      event = rep(1, 9),
