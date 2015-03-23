f9d7e2898fbe3b984fd60f1603059b1700175a06
Benjamin <benjak@math.su.se>
2015-03-23 07:53:35 +0100

    Fixed bug+updated test. Check later if logsumexp is needed for accuracy.

--- tests/testthat/test_mbss_likelihood_ratios.R
+++ tests/testthat/test_mbss_likelihood_ratios.R
@@ -7,11 +7,11 @@ test_that("spacetime_llr: sums correctly", {
                   event = rep(1, 24),
                   time = c(rep(0:2, 2, each = 2), rep(0:2, each = 4)),
                   stream = c(rep(1:2, 6), rep(1:2, 3, each = 2)),
-                  llr = 1:24)
+                  llr = log(1:24))
   setkeyv(y, c("region", "event", "time", "stream", "location"))
   
   stllr <- spacetime_llr(y)
-  expect_equal(stllr[, llr], c(3, 10, 21, 15, 34, 57, 58, 132, 222))
+  expect_equal(stllr[, exp(llr)], c(3, 10, 21, 15, 34, 57, 58, 132, 222))
 })
 
 test_that("event_llr: calculates correctly", {
