b46b28f7d2969d8b7c82b9902a8daa6a12459847
Benjamin <benjak@math.su.se>
2015-03-26 22:30:48 +0100

    Removed clutter.

--- tests/testthat/test_mbss_probability_map.R
+++ tests/testthat/test_mbss_probability_map.R
@@ -25,16 +25,9 @@ test_that("event_logprobability_map: calculated correctly", {
                     posterior_logprob = log(c(0.5, 0.5, 2.5, 2.5, 0.5, 1.5)))
   setkeyv(lps, c("region", "event"))
   
-  lse <- function(x) log(sum(exp(x)))
   regions <- data.table(location = rep(1:2, 2), region = c(1:3, 3),
                         key = "region")
-  elpm <- event_logprobability_map(lps, regions)
-  expect_equal(elpm[, posterior_logprob], 
-               vapply(list(log(c(0.5, 0.5)),
-                           log(c(0.5, 1.5)),
-                           log(c(2.5, 0.5)),
-                           log(c(2.5, 1.5))),
-                      lse,
-                      numeric(1)),
-               log(1:4))
+  
+  actual <- event_logprobability_map(lps, regions)[, posterior_logprob]
+  expect_equal(actual, log(1:4))
 })
