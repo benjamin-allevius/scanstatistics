d43ea16d06034ec826db66ccb52f0bd711a8a148
Benjamin <benjak@math.su.se>
2015-03-31 22:27:26 +0200

    Need more extraction functions and pretty printing.

--- R/mbss.R
+++ R/mbss.R
@@ -1,25 +1,51 @@
-# TODO: documentation
-# Change input to densities instead of likelihood ratios
-# Work out how to convert back to actual times 
-# - pass in observed times and counts?
 
-MBSS <- function(counts_and_logdensities,
+#' Calculate posterior event probabilities for all regions and event durations.
+#' 
+#' Calculate the posterior probability of an event for each event type,
+#' region (group of one or more locations), and event duration.
+#' Assumes that the prior probability of a region being affected by a given
+#' event is uniform over all regions, and that events are mutually exclusive
+#' and affect only a single region.
+#' 
+#' @param loglikelihoods A \code{data.table} in long format,
+#'        containing at least the columns
+#'        \code{event, stream, location, time, null_loglikelihood} and
+#'        \code{event_loglikelihood}. The column \code{null_loglikelihood}
+#'        contains the predicted log-likelihood for each observation 
+#'        (i.e.\ the observation made for a particular data stream, 
+#'        at a particular location, at a particular time) 
+#'        under the null hypothesis of no event. These values will be duplicated
+#'        across the different event types.
+#'        The column \code{event_loglikelihood} contains the predicted 
+#'        log-likelihood for each observation given that an event of the 
+#'        specified type occurs.
+#' @param regions A \code{set} of regions, each region itself a set containing
+#'        one or more locations of those found in the table 
+#'        \code{loglikelihoods}.
+#' @param null_prior The prior probability that no event has taken place.
+#' @param event_priors The prior probabilities of each event, given as a vector.
+#' @param duration_condpriors The prior conditional probabilities of the event
+#'        durations, given the event types. Either the string 'uniform',
+#'        meaning that the event durations are uniformly distributed for all
+#'        event types, or a matrix in which the row numbers correspond to the
+#'        event duration, and the columns to the different events.
+MBSS <- function(loglikelihoods,
                  regions,
                  null_prior,
                  event_priors,
                  duration_condpriors = "uniform") {
   # Input checking -------------------------------------------------------------
-  if (!is.data.table(counts_and_logdensities)) {
-    stop("The log-likelihood ratios must be supplied as a data.table.")
+  if (!is.data.table(loglikelihoods)) {
+    stop("The log-likelihoods must be supplied as a data.table.")
   }
-  if (!all(c("event", "stream", "location", "time", "count", 
-             "null_logdensity", "event_logdensity") %in% 
-             names(counts_and_logdensities))) {
-    stop("The data.table containing the log-likelihood ratios must ",
-         "contain the columns 'event', 'stream', 'location', 'time', 'count', ",
-         "'null_logdensity', 'event_logdensity'.")
+  if (!all(c("event", "stream", "location", "time", 
+             "null_loglikelihood", "event_loglikelihood") %in% 
+             names(loglikelihoods))) {
+    stop("The data.table containing the log-likelihood must ",
+         "contain the columns 'event', 'stream', 'location', 'time', ",
+         "'null_loglikelihood', 'event_loglikelihood'.")
   }
-  if (any(is.na(counts_and_logdensities))) {
+  if (any(is.na(loglikelihoods))) {
     stop("No support for missing values yet. Consider imputation methods.")
   }
   if (length(null_prior) != 1 || !is.numeric(null_prior) || null_prior < 0 ||
@@ -42,8 +68,13 @@ MBSS <- function(counts_and_logdensities,
     stop("Conditional probabilities for event durations given event type ",
          "must sum to the event priors.")
   }
+  if (!is.character(duration_condpriors) && 
+        ncol(duration_condpriors) != length(event_priors)) {
+    stop("The number of event prior probabilities must equal the ",
+         "number of events found in the event duration priors.")
+  }
   # Proceed with calculations --------------------------------------------------
-  times_durations <- times_and_durations(counts_and_logdensities)
+  times_durations <- times_and_durations(loglikelihoods)
   n_regions <- length(regions)
   n_events <- length(event_priors)
   max_duration <- max(times_durations[, duration])
@@ -55,14 +86,13 @@ MBSS <- function(counts_and_logdensities,
                                   ncol = n_events)
   }
   
-  null_loglikelihood <- 
-    counts_and_logdensities[event == counts_and_logdensities[1, event],
-                            sum(null_logdensity)]
+  null_loglikelihood <- loglikelihoods[event == loglikelihoods[1, event],  
+                                       sum(null_loglikelihood)]
 
   # Calculate log-likelihood ratios for all events, regions, and durations
   keys_used_for_stllr <- c("region", "event", "duration", "stream", "location")
   spacetime_output <- 
-    counts_and_logdensities[, .(llr = event_logdensity - null_logdensity),
+    loglikelihoods[, .(llr = event_loglikelihood - null_loglikelihood),
       keyby = .(location, event, stream, time)] %>%
     add_duration %>%
     region_joiner(regions = regions, keys = keys_used_for_stllr) %>%
@@ -107,7 +137,7 @@ MBSS <- function(counts_and_logdensities,
     warning("Posterior probabilities don't sum to 1.")
   }
   
-  structure(list(counts_and_logdensities = counts_and_logdensities,
+  structure(list(loglikelihoods = loglikelihoods,
                  max_duration = max_duration,
                  time_and_durations = times_durations,
                  n_regions = n_regions,
@@ -123,4 +153,50 @@ MBSS <- function(counts_and_logdensities,
                  event_probability_map = event_pmap,
                  probability_map = pmap), 
             class = "MBSS")
+}
+
+#' Extract the posterior conditional event duration probabilities from an MBSS
+#' object.
+#' 
+#' @param MBSS_object An object of class "MBSS".
+#' @param duration_column A logical scalar. Should the output matrix
+#'        contain a column for the event duration (elements being integers
+#'        equal to the row numbers)?
+get_duration_condposteriors <- function(MBSS_obj, duration_column = TRUE) {
+  out <- tidyr::spread(MBSS_obj$duration_condposteriors, 
+                       key = event, 
+                       value = duration_condposterior)
+  out <- as.matrix(out)
+  if (duration_column) {
+    return(out)
+  } else {
+    return(out[, -1])
+  }
+}
+
+#' Get the k most probable region-event type-duration combinations.
+#' 
+#' Extracts the k most probable combinations of region, event type and event
+#' duration. One can also specify a restriction to a subset of the event,
+#' and/or a subset of the durations.
+get_k_most_probable <- function(MBSS_obj, 
+                                k = 10, 
+                                events = "all", 
+                                durations = "all") {
+  if (is.character(events) && events == "all" && 
+      is.character(durations) && durations == "all") {
+    MBSS_obj$posteriors[order(-posterior_prob), 
+      .(region, event, duration, posterior_prob)][seq(k)]
+  } else if (is.character(durations) && durations == "all") {
+    MBSS_obj$posteriors[event %in% events,
+      .(region, event, duration, posterior_prob)][
+      order(-posterior_prob)][seq(k)]
+  } else if (is.character(events) && events == "all") {
+    MBSS_obj$posteriors[duration %in% durations,
+      .(region, event, duration, posterior_prob)][order(-posterior_prob)][seq(k)]
+  } else {
+    MBSS_obj$posteriors[event %in% events & duration %in% durations,
+      .(region, event, duration, posterior_prob)][
+      order(-posterior_prob)][seq(k)]
+  }
 }
--- tests/testthat/test_mbss.R
+++ tests/testthat/test_mbss.R
@@ -8,22 +8,22 @@ times <- seq(as.POSIXct("2015-03-27 11:00:00 CET"),
 locations <- 1:2
 streams <- 1:2
 events <- 1:2
-counts_and_logdensities <- table_creator(list(time = times, 
-                                              location = locations,
-                                              stream = streams,
-                                              event = events))
+loglikelihoods <- table_creator(list(time = times, 
+                                     location = locations,
+                                     stream = streams,
+                                     event = events))
 # Time-dependent rate
-null_rate <- counts_and_logdensities[event == 1, 
+null_rate <- loglikelihoods[event == 1, 
   5*stream + location + 
     4*sin((lubridate::hour(time) + lubridate::minute(time)/4) / (2*pi*24))]
 
 # Fill with null and event likelihoods
-counts_and_logdensities[, 
+loglikelihoods[, 
   count := rep(rpois(length(null_rate), lambda = null_rate), 2)]
-counts_and_logdensities[, 
-  null_logdensity := dpois(count, lambda = rep(null_rate, 2), log = TRUE)]
-counts_and_logdensities[, 
-  event_logdensity := dpois(count, lambda = null_rate + rep(1:2, each = 20), 
+loglikelihoods[, 
+  null_loglikelihood := dpois(count, lambda = rep(null_rate, 2), log = TRUE)]
+loglikelihoods[, 
+  event_loglikelihood := dpois(count, lambda = null_rate + rep(1:2, each = 20), 
                             log = TRUE)]
 
 null_prior <- 0.9
@@ -36,7 +36,7 @@ regions <- sets::set(sets::as.set(1L), sets::as.set(2L), sets::as.set(1:2))
 # Tests ------------------------------------------------------------------------
 
 test_that("MBSS: works for valid input", {
-  m <- MBSS(counts_and_logdensities, 
+  m <- MBSS(loglikelihoods, 
             regions, 
             null_prior, 
             event_priors, 
@@ -46,7 +46,7 @@ test_that("MBSS: works for valid input", {
 })
 
 test_that("MBSS: works for valid input (uniform duration)", {
-  m <- MBSS(counts_and_logdensities, 
+  m <- MBSS(loglikelihoods, 
             regions, 
             null_prior, 
             event_priors, 
@@ -56,7 +56,7 @@ test_that("MBSS: works for valid input (uniform duration)", {
 })
 
 test_that("MBSS: throws error when wrong column name", {
-  cal <- copy(counts_and_logdensities)
+  cal <- copy(loglikelihoods)
   setnames(cal, "time", "invalid")
   expect_error(MBSS(cal, 
                     regions, 
@@ -67,7 +67,7 @@ test_that("MBSS: throws error when wrong column name", {
 })
 
 test_that("MBSS: throws error when wrong column name", {
-  cal <- copy(counts_and_logdensities)
+  cal <- copy(loglikelihoods)
   setnames(cal, "time", "invalid")
   expect_error(MBSS(cal, 
                     regions, 
@@ -78,12 +78,13 @@ test_that("MBSS: throws error when wrong column name", {
 })
 
 test_that("MBSS: throws error when NA present", {
-  cal <- copy(counts_and_logdensities)
-  cal[1, ]
+  cal <- copy(loglikelihoods)
+  cal[1, null_loglikelihood := NA]
   expect_error(MBSS(cal, 
                     regions, 
                     null_prior, 
                     event_priors, 
-                    "uniform"))
+                    "uniform"),
+               regexp = "No support for missing values yet.")
   
 })
