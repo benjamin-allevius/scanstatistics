6d5ac74657e6bae2e73c3983f8fb6f3b3a7e14fc
Benjamin <benjak@math.su.se>
2015-04-03 14:01:34 +0200

    Added functionality test.

--- tests/testthat/test_utility_functions.R
+++ tests/testthat/test_utility_functions.R
@@ -1,5 +1,6 @@
 context("Utility functions")
 
+# Functions to do with data.table keys -----------------------------------------
 test_that("key is returned if present", {
   DT <- data.table(x = 1:3, y = 1:3, key = "x")
   expect_equal(getkeys(DT), "x")
@@ -39,4 +40,24 @@ test_that("FALSE if no keys", {
 test_that("FALSE if checking too many keys", {
   DT <- data.table(x = 1:3, y = 1:3, key = c("x", "y"))
   expect_false(first_keys_are_equal(DT, c("x", "y", "z")))
+})
+
+# Convenience ------------------------------------------------------------------
+test_that("add_llr: works correctly", {
+  n_times <- 2
+  times <- seq(as.POSIXct("2015-03-27 11:00:00 CET"), 
+               length.out = n_times, by = "15 mins")
+  locations <- 1:2
+  streams <- 1:2
+  events <- 0:2
+  loglikelihoods <- table_creator(list(time = times, 
+                                       location = locations,
+                                       stream = streams,
+                                       event = events))
+  setkeyv(loglikelihoods, c("event", "location", "stream", "time"))
+  loglikelihoods[, loglikelihood := 1:24]
+  expected <- loglikelihoods[event != 0][, 
+    llr := loglikelihood - rep(1:8, 2)]
+  setkeyv(expected, c("location", "event", "stream", "time"))
+  expect_equal(add_llr(loglikelihoods, 0)[, llr], expected[, llr])
 })
