70149d2304a35306a4329ab316db59d03b8fd4cb
Benjamin <benjak@math.su.se>
2015-04-04 23:12:06 +0200

    Simple test for initial aggregates for EBG.

--- tests/testthat/test_fss_score.R
+++ tests/testthat/test_fss_score.R
@@ -13,6 +13,18 @@ test_that("aggregate_CB_poisson: works as intended", {
 })
 
 
+test_that("aggregate_CB_gaussian: works as intended", {
+  counts <- data.table(location = rep(1:2, each = 4),
+                       stream = rep(1:2, 2, each = 2),
+                       duration = rep(1:2, 4))
+  counts[, count := 1:8]
+  counts[, baseline := 1:8]
+  counts[, variance := 1:8]
+  CBim <- aggregate_CB_gaussian(counts)
+  expect_equal(CBim[, aggregate_count], c(1, 3, 3, 7, 5, 11, 7, 15))
+  expect_equal(CBim[, aggregate_baseline], c(1, 3, 3, 7, 5, 11, 7, 15))
+})
+
 
 
 
