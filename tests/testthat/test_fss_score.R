6507e10a44097dc9c4507263d41940bd71998cb0
Benjamin <benjak@math.su.se>
2015-04-04 23:12:22 +0200

    Simple test for initial aggregates for EBE.

--- tests/testthat/test_fss_score.R
+++ tests/testthat/test_fss_score.R
@@ -25,6 +25,16 @@ test_that("aggregate_CB_gaussian: works as intended", {
   expect_equal(CBim[, aggregate_baseline], c(1, 3, 3, 7, 5, 11, 7, 15))
 })
 
+test_that("aggregate_CB_exponential: works as intended", {
+  counts <- data.table(location = rep(1:2, each = 4),
+                       stream = rep(1:2, 2, each = 2),
+                       duration = rep(1:2, 4))
+  counts[, count := 1:8]
+  counts[, baseline := 1:8]
+  CBim <- aggregate_CB_exponential(counts)
+  expect_equal(CBim[, aggregate_count], rep(1:2, 4))
+  expect_equal(CBim[, aggregate_baseline], rep(1:2, 4))
+})
 
 
 
