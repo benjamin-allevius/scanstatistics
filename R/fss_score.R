5b801b1950292db865044a64c739560eeac7419c
Benjamin <benjak@math.su.se>
2015-04-07 22:16:11 +0200

    Rearranged code by distribution used.

--- R/fss_score.R
+++ R/fss_score.R
@@ -1,52 +1,6 @@
 
-#' Calculate the aggregate counts and baselines for the EBP scan statistic
-#' over all event durations.
-#' 
-#' Calculate the aggregate counts \eqn{C_{i,m}} and aggregate baselines 
-#' \eqn{B_{i,m}} for the expectation-based Poisson (EBP) scan statistic, for 
-#' each location \eqn{i} and data stream \eqn{m}. I.e. the cumulative sum over
-#' the event duration, from shortest to longest, is calculated.
-#' 
-#' @param counts A \code{data.table} with columns \code{location, stream, 
-#'        duration, count, baseline}.
-aggregate_CB_poisson <- function(counts) {
-  counts[, .(duration = duration,
-             aggregate_count = cumsum(count),
-             aggregate_baseline = cumsum(baseline)),
-         by = .(location, stream)]
-}
 
-#' Calculate the aggregate counts and baselines for the EBG scan statistic
-#' over all event durations.
-#' 
-#' Calculate the aggregate counts \eqn{C_{i,m}} and aggregate baselines 
-#' \eqn{B_{i,m}} for the expectation-based Gaussian (EBG) scan statistic, for 
-#' each location \eqn{i} and data stream \eqn{m}. I.e. the cumulative sum over
-#' the event duration, from shortest to longest, is calculated.
-#' 
-#' @inheritParams aggregate_CB_poisson
-aggregate_CB_gaussian <- function(counts) {
-  counts[, .(duration = duration,
-             aggregate_count = cumsum(count * baseline / variance),
-             aggregate_baseline = cumsum(baseline^2 / variance)),
-         by = .(location, stream)]
-}
-
-#' Calculate the aggregate counts and baselines for the EBE scan statistic
-#' over all event durations.
-#' 
-#' Calculate the aggregate counts \eqn{C_{i,m}} and aggregate baselines 
-#' \eqn{B_{i,m}} for the expectation-based Exponential (EBE) scan statistic, for 
-#' each location \eqn{i} and data stream \eqn{m}. I.e. the cumulative sum over
-#' the event duration, from shortest to longest, is calculated.
-#' 
-#' @inheritParams aggregate_CB_poisson
-aggregate_CB_exponential <- function(counts) {
-  counts[, .(duration = duration,
-             aggregate_count = cumsum(count / baseline),
-             aggregate_baseline = duration), 
-         by = .(location, stream)]
-}
+# General functions ------------------------------------------------------------
 
 #' Calculate the count and baseline aggregates over each region-stream-duration
 #' combination.
@@ -68,6 +22,42 @@ aggregate_again <- function(aggregates) {
              by = .(region, duration, stream)]
 }
 
+#' Score and minimal stream subset for the Naive Kulldorff method.
+#' 
+#' Calculates the score for each combination of region and duration, and the 
+#' minimal subset of locations that contribute to the score, according to the
+#' Naive Kulldorff method. See Neill et. al. (2013) section 3.2.
+#' 
+#' @param scores A \code{data.table} with columns \code{region, duration, stream, 
+#'        score}.
+#' @return A \code{data.table} with columns \code{region, duration, stream, 
+#'         score, included_streams}. The colum \code{score} contain the sum
+#'         of the input score over all data streams, for each region and 
+#'         duration. The column \code{included_streams} contain those data 
+#'         streams that made a positive contribution to this sum.
+score_minimal_stream_subset <- function(scores) {
+  scores[score > 0, 
+         .(score = sum(score), included_streams = list(stream)), 
+         by = .(region, duration)]
+}
+# Expectation-based Poisson ----------------------------------------------------
+
+#' Calculate the aggregate counts and baselines for the EBP scan statistic
+#' over all event durations.
+#' 
+#' Calculate the aggregate counts \eqn{C_{i,m}} and aggregate baselines 
+#' \eqn{B_{i,m}} for the expectation-based Poisson (EBP) scan statistic, for 
+#' each location \eqn{i} and data stream \eqn{m}. I.e. the cumulative sum over
+#' the event duration, from shortest to longest, is calculated.
+#' 
+#' @param counts A \code{data.table} with columns \code{location, stream, 
+#'        duration, count, baseline}.
+aggregate_CB_poisson <- function(counts) {
+  counts[, .(duration = duration,
+             aggregate_count = cumsum(count),
+             aggregate_baseline = cumsum(baseline)),
+         by = .(location, stream)]
+}
 
 #' Calculates the score for the expectation-based Poisson scan statistic.
 #' 
@@ -95,26 +85,6 @@ score_EBP <- function(aggregates) {
              by = .(region, duration, stream)]
 }
 
-#' Score and minimal stream subset for the Naive Kulldorff method.
-#' 
-#' Calculates the score for each combination of region and duration, and the 
-#' minimal subset of locations that contribute to the score, according to the
-#' Naive Kulldorff method. See Neill et. al. (2013) section 3.2.
-#' 
-#' @scores A \code{data.table} with columns \code{region, duration, stream, 
-#'         score}.
-#' @return A \code{data.table} with columns \code{region, duration, stream, 
-#'         score, included_streams}. The colum \code{score} contain the sum
-#'         of the input score over all data streams, for each region and 
-#'         duration. The column \code{included_streams} contain those data 
-#'         streams that made a positive contribution to this sum.
-score_minimal_stream_subset <- function(scores) {
-  scores[score > 0, 
-         .(score = sum(score), included_streams = list(stream)), 
-         by = .(region, duration)]
-}
-
-
 #' Calculate the scores according to the Naive Kulldorff method.
 #' @importFrom magrittr %>%
 naive_kulldorff_poisson <- function(counts, regions) {
@@ -126,3 +96,43 @@ naive_kulldorff_poisson <- function(counts, regions) {
     score_EBP %>%
     score_minimal_stream_subset
 }
+
+
+# Expectation-based Gaussian ---------------------------------------------------
+
+#' Calculate the aggregate counts and baselines for the EBG scan statistic
+#' over all event durations.
+#' 
+#' Calculate the aggregate counts \eqn{C_{i,m}} and aggregate baselines 
+#' \eqn{B_{i,m}} for the expectation-based Gaussian (EBG) scan statistic, for 
+#' each location \eqn{i} and data stream \eqn{m}. I.e. the cumulative sum over
+#' the event duration, from shortest to longest, is calculated.
+#' 
+#' @inheritParams aggregate_CB_poisson
+aggregate_CB_gaussian <- function(counts) {
+  counts[, .(duration = duration,
+             aggregate_count = cumsum(count * baseline / variance),
+             aggregate_baseline = cumsum(baseline^2 / variance)),
+         by = .(location, stream)]
+}
+
+
+# Expectation-based Exponential ------------------------------------------------
+
+#' Calculate the aggregate counts and baselines for the EBE scan statistic
+#' over all event durations.
+#' 
+#' Calculate the aggregate counts \eqn{C_{i,m}} and aggregate baselines 
+#' \eqn{B_{i,m}} for the expectation-based Exponential (EBE) scan statistic, for 
+#' each location \eqn{i} and data stream \eqn{m}. I.e. the cumulative sum over
+#' the event duration, from shortest to longest, is calculated.
+#' 
+#' @inheritParams aggregate_CB_poisson
+aggregate_CB_exponential <- function(counts) {
+  counts[, .(duration = duration,
+             aggregate_count = cumsum(count / baseline),
+             aggregate_baseline = duration), 
+         by = .(location, stream)]
+}
+
+
