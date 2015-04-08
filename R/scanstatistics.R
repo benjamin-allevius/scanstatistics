d7f1bd54f66bd03176987df6ea75e45242afc9b1
Benjamin <benjak@math.su.se>
2015-04-08 21:10:32 +0200

    Fix for R CMB notes.

--- R/scanstatistics.R
+++ R/scanstatistics.R
@@ -15,3 +15,46 @@ NULL
 # To avoid problems with devtools::test()
 # See https://github.com/hadley/devtools/issues/192
 .datatable.aware=TRUE
+
+# Hack based on Hadley Wickhams comment: 
+# http://stackoverflow.com/a/12429344/897506
+globalVariables(c(# Variables used unquoted inside functions
+  "duration", 
+  "event",
+  "location",
+  "stream",
+  "region",
+  "posterior_prob",
+  "posterior_logprob",
+  "event_posterior",
+  "llr",
+  "rs",
+  "count",
+  "baseline",
+  "overdispersion",
+  "effect_logprob",
+  "duration_condposterior",
+  "num",
+  "denom",
+  "timesum",
+  "locsum",
+  "prop",
+  "duration_event_posterior",
+  "regions_in_part",
+  "totalsum",
+  "loglikelihood",
+  "variance",
+  "aggregate_count",
+  "aggregate_baseline",
+  "score",
+  # data.table functions
+  "data.table",
+  "is.data.table",
+  ".",
+  ":=",
+  "setkeyv",
+  ".SD",
+  # foreach
+  "%dopar%",
+  "foreach"),
+  package = "scanstatistics")
