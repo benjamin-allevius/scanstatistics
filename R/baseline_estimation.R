217ddb59993b786f17b4e26bda8ac7fffdaaf576
Benjamin <benjak@math.su.se>
2015-04-17 22:33:28 +0200

    Input validation.

--- R/baseline_estimation.R
+++ R/baseline_estimation.R
@@ -10,6 +10,10 @@
 #' @return A \code{data.table} with columns \code{stream, location, time, count, 
 #'    baseline}. Key columns are \code{stream, location, time} in that order.
 kulldorff_baseline <- function(counts) {
+  key_order <- c("stream", "location", "time")
+  if (!all(getkeys(counts)[1:3] == key_order)) {
+    stop("Key columns of input table must be 'stream', 'location', 'time'.")
+  }
   sum_by_time <- counts[, .(timesum = sum(count)), keyby = .(stream, location)]
   sum_by_loc <- counts[, .(locsum = sum(count)), keyby = .(stream, time)]
   sum_by_both <- counts[, .(totalsum = sum(count)), keyby = "stream"]
@@ -20,6 +24,6 @@ kulldorff_baseline <- function(counts) {
                      by = c("stream"), allow.cartesian = TRUE)[, 
                  .(stream = stream, location = location, time = time, 
                    baseline = locsum * prop)]
-  setkeyv(baselines, c("stream", "location", "time"))
+  setkeyv(baselines, key_order)
   merge(counts, baselines)
 }
