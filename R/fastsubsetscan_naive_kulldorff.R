fa349e85401ad7813062ef2e28dbd7523fc1a39b
Benjamin <benjak@math.su.se>
2015-04-23 19:49:51 +0200

    Removed distribution-dependent functions, made single dispatch function.

--- R/fastsubsetscan_naive_kulldorff.R
+++ R/fastsubsetscan_naive_kulldorff.R
@@ -2,70 +2,37 @@
 #' Calculates the multivariate scan statistic by the naive Kulldorff method.
 #' 
 #' Calculates the multivariate scan statistic by the naive Kulldorff method, for 
-#' all stream-region-duration combinations, according to the distribution used 
-#' in the supplied functions.
+#' all stream-region-duration combinations, according to the given distribution.
 #' @param counts A \code{data.table} with columns \code{region, duration, 
 #'    stream, count, baseline}, and possibly others.
+#' @param distribution The distribution to be used; one of "poisson", "gaussian"
+#'    (or "normal"), or "exponential".
 #' @param regions A \code{list} or \code{set} of regions, 
 #'    each region itself a set containing one or more locations of those found 
 #'    in \code{counts}.
-#' @inheritParams region_apply
-#' @param initial_aggregation A function for calculating the aggregate counts and 
-#'    baselines over all event durations for a given expectation-based scan 
-#'    statistic.
-#' @inheritParams expectation_based_score
+#' @inheritParams MBSS
 #' @return A \code{data.table} with columns \code{region, duration, score,
 #'    included_streams}.
 #' @importFrom magrittr %>%
-naive_kulldorff_general <- function(counts, 
-                                    regions, 
-                                    region_partition,
-                                    initial_aggregation, 
-                                    score_function) {
+naive_kulldorff <- function(counts, 
+                            distribution,
+                            regions, 
+                            n_partitions = "auto") {
+  if (n_partitions == "auto") {
+    n_partitions <- auto_region_partition_size(regions)
+  }
+  region_partition <- partition_regions(regions, n_parts = n_partitions)
+  
+  # Define aggregation and score functions based on distribution
+  initial_aggregation_fun <- dispatch_initial_aggregation(distribution)
+  score_fun <- dispatch_score_function(distribution)
+  
   counts %>%
-    initial_aggregation %>%
+    initial_aggregation_fun %>%
     region_apply(region_partition = region_partition,
                  f = aggregate_per_stream,
                  keys = c("region", "duration", "stream")) %>%
-    expectation_based_score(score_function = score_function,
+    expectation_based_score(score_function = score_fun,
                             region_as_list = FALSE) %>%
     score_minimal_stream_subset(region_as_list = FALSE)
-}
-
-# Expectation-based Poisson ----------------------------------------------------
-
-#' Calculates the EBP multivariate scan statistic by the naive Kulldorff method.
-#' 
-#' This function calculates the multivariate scan statistic by the naive 
-#' Kulldorff method, using the expectation-based Poisson score function.
-#' @inheritParams naive_kulldorff_general
-naive_kulldorff_poisson <- function(counts, regions, region_partition) {
-  naive_kulldorff_general(
-    counts, regions, region_partition, initial_aggregation_poisson, score_fun_EBP)
-}
-
-
-# Expectation-based Gaussian ---------------------------------------------------
-
-#' Calculates the EBG multivariate scan statistic by the naive Kulldorff method.
-#' 
-#' This function calculates the multivariate scan statistic by the naive 
-#' Kulldorff method, using the expectation-based Gaussian score function.
-#' @inheritParams naive_kulldorff_general
-naive_kulldorff_gaussian <- function(counts, regions, region_partition) {
-  naive_kulldorff_general(
-    counts, regions, region_partition, initial_aggregation_gaussian, score_fun_EBG)
-}
-
-
-# Expectation-based Exponential ------------------------------------------------
-
-#' Calculates the EBE multivariate scan statistic by the naive Kulldorff method.
-#' 
-#' This function calculates the multivariate scan statistic by the naive 
-#' Kulldorff method, using the expectation-based exponential score function.
-#' @inheritParams naive_kulldorff_general
-naive_kulldorff_exponential <- function(counts, regions, region_partition) {
-  naive_kulldorff_general(
-    counts, regions, region_partition, initial_aggregation_exponential, score_fun_EBE)
 }
