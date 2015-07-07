03e82ae1daf29d3df90a3ffa74aaef4619b2a64e
Benjamin <benjak@math.su.se>
2015-07-07 10:56:16 +0200

    Possibly needs more thorough tests.

--- tests/testthat/test_poisson_expectation.R
+++ tests/testthat/test_poisson_expectation.R
@@ -1,6 +1,21 @@
 context("Expectation-Based Poisson Scan Statistic")
 
 
+test_that("poisson_mcsim", {
+  table <- table_creator(list(location = 1:2, duration = 1:3), 
+                         keys = c("location", "duration"))
+  table[, mean := 1:6 + 0.5]
+  table[, count := c(1,3,2, 7, 3, 10)]
+  regions <- sets::set(sets::as.set(1L), 
+                       sets::as.set(2L),
+                       sets::as.set(1:2))
+  
+  nsims <- 10
+  actual <- poisson_mcsim(table, regions, nsims)
+  expect_true(!any(actual < 0))
+  expect_true(length(actual) == nsims)
+})
+
 test_that("poisson_statistic: calculates correctly", {
   table <- table_creator(list(region = 1:3, duration = 1:2), 
                          keys = c("region", "duration"))
