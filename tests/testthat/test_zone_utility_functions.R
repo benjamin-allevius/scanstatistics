34194c54fda279fd2b77c0be98c173a2a8eb8de4
Benjamin <benjak@math.su.se>
2016-07-08 09:40:13 +0200

    Rename region --> zone.

--- tests/testthat/test_zone_utility_functions.R
+++ tests/testthat/test_zone_utility_functions.R
@@ -1,1 +1,17 @@
-context("Zone utility functions")
+context("zone utility functions")
+
+# zones <- sets::set(sets::as.set(1L),
+#                      sets::as.set(2L),
+#                      sets::as.set(3L),
+#                      sets::as.set(1:2),              
+#                      sets::as.set(c(1L, 3L)),
+#                      sets::as.set(c(2L, 3L)))
+# 
+# 
+# zones <- sets::set(sets::as.set(1L),
+#                      sets::as.set(2L),
+#                      sets::as.set(3L),
+#                      sets::as.set(1:2),              
+#                      sets::as.set(c(1L, 3L)),
+#                      sets::as.set(c(2L, 3L)),
+#                      sets::as.set(c(1L, 4L)))
