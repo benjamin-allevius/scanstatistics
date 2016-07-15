50a2f39dc8a56f4fa40a8d5d785d9d97a5c3078b
Benjamin <benjak@math.su.se>
2016-07-15 23:54:51 +0200

    Need finish details and add example.

--- R/scan_zip.R
+++ R/scan_zip.R
@@ -1,46 +1,97 @@
 # Main function ----------------------------------------------------------------
 
-#' Calculate the spatiotemporal ZIP scan statistic.
+#' Calculate the ZIP scan statistic.
 #' 
-#' This function calculates the spatiotemporal zero-inflated Poisson scan 
-#' statistic, along with the most likely cluster (MLC) and its Monte Carlo 
-#' \eqn{p}-value. See details.
-#' @param table A \code{data.table} with columns \code{duration, location, 
-#'    count} and columns for distribution parameters that match the chosen type
-#'    of scan statistic. See details.
-#' @param zones A \code{set} of \code{set}s, the inner sets containing integer
-#'    values that match those in the column \code{location} of the \code{table}
-#'    argument of this function. See details.
-#' @param n_replicates The number of Monte Carlo replications to perform in 
-#'    order to calculate a p-value.
+#' Calculate the expectation-based Poisson scan statistic by supplying a 
+#' \code{data.table} of observed counts and pre-computed expected value 
+#' parameters for each location and time. A p-value for the observed scan
+#' statistic can be obtained by Monte Carlo simulation.
+#' 
+#' @param table A \code{data.table} with columns 
+#'    \code{location, duration, count, mean, p}. The \code{location} column 
+#'    should consist of integers that are unique to each location. The 
+#'    \code{duration} column should also consist of integers, starting at 1 for 
+#'    the most recent time period and increasing in reverse chronological order.
+#'    The column \code{mean} should contain the estimated Poisson expected value 
+#'    parameter, and the column \code{p} the estimated structural zero 
+#'    probability.
+#' @param zones A \code{set} of zones, each zone itself a set containing one or 
+#'    more locations of those found in \code{table}.
+#' @param n_mcsim A non-negative integer; the number of replicate scan 
+#'    statistics to generate in order to calculate a p-value.
 #' @param ... Arguments passed to internal functions. Arguments that can be
 #'    passed here are \code{d}, the initial value for the excess zero indicators
 #'    (default is 0.5), and \code{tol}, the threshold for the absolute 
 #'    convergence criterion (default is 0.01).
+#' @return An object of class \code{scanstatistics}. It has the following 
+#'    fields:
+#'    \describe{
+#'     \item{observed}{A \code{data.table} containing the value of the 
+#'                     statistic calculated for each zone-duration combination,
+#'                     for the observed data. The scan statistic is the maximum
+#'                     value of these calculated statistics.}
+#'     \item{replicated}{A numeric vector of length \code{n_mcsim} containing 
+#'                       the values of the scanstatistics calculated by Monte
+#'                       Carlo simulation.}
+#'     \item{mlc}{A \code{data.table} containing the zone, duration, and 
+#'                scanstatistic.}
+#'     \item{pvalue}{The p-value calculated from Monte Carlo replications.}
+#'     \item{distribution}{The assumed distribution of the data; "zero-inflated 
+#'                         Poisson" in this case.}
+#'     \item{type}{The type of scan statistic; "Expectation-based" in this 
+#'                 case.}
+#'     \item{zones}{The set of zones that was passed to the function as input.}
+#'     \item{n_locations}{The number of locations in the data.}
+#'     \item{n_zones}{The number of zones.}
+#'     \item{max_duration}{The maximum anomaly duration considered.}
+#'    }
 #' @export
-zip_scanstatistic <- function(table, zones, n_replicates, ...) {
-  # input validation
-  
-  # Calculate statistics for observed data
-  # extract max value
+#' @concept poisson scanstatistic
+#' @details For the expectation-based zero-inflated Poisson scan statistic 
+#'    (Kjellson 2015), the null hypothesis of no anomaly holds that the count 
+#'    observed at each location \eqn{i} and duration \eqn{t} (the number of time 
+#'    periods before present) has a zero-inflated Poisson distribution with 
+#'    expected value parameter \eqn{\mu_{it}} and structural zero probability
+#'    \eqn{p_{it}}:
+#'    \deqn{
+#'      H_0 : Y_{it} \sim \textrm{ZIP}(\mu_{it}, p_{it}).
+#'    }
+#'    This holds for all locations \eqn{i = 1, \ldots, m} and all durations 
+#'    \eqn{t = 1, \ldots,T}, with \eqn{T} being the maximum duration considered.
+#'    Under the alternative hypothesis, there is a space-time window \eqn{W}
+#'    consisting of a spatial zone \eqn{Z \subset \{1, \ldots, m\}} and a time 
+#'    window \eqn{D \subseteq \{1, \ldots, T\}} such that the counts in that
+#'    window have their Poisson expected value parameters inflated by a factor 
+#'    \eqn{q_W > 1} compared to the null hypothesis:
+#'    \deqn{
+#'    H_1 : Y_{it} \sim \textrm{ZIP}(q_W \mu_{it}, p_{it}), ~~(i,t) \in W.
+#'    }
+#'    For locations and durations outside of this window, counts are assumed to
+#'    be distributed as under the null hypothesis. The sets \eqn{Z} considered 
+#'    are those specified in the argument \code{zones}, while the maximum 
+#'    duration \eqn{T} is taken as the maximum value in the column 
+#'    \code{duration} of the input \code{table}. For each space-time window
+#'    \eqn{W} considered, (the log of) a likelihood ratio is computed using the 
+#'    distributions under the alternative and null hypotheses, and the 
+#'    expectation-based Poisson scan statistic is calculated as the maximum of 
+#'    these quantities over all space-time windows.
+#'    Point estimates of the parameters \eqn{\mu_{it}} must be specified in the
+#'    column \code{mean} of the argument \code{table} before this function is 
+#'    called.
+#' @references 
+#'    Kjellson, B. (2015), \emph{Spatiotemporal Outbreak Detection A Scan 
+#'    Statistic Based on the Zero-Inflated Poisson Distribution}, (Master 
+#'    Thesis, Stockholm University),
+#'    \href{http://goo.gl/6Q89ML}{Link to PDF}.
+#' @examples 
+scan_zip <- function(table, zones, n_mcsim = 0, ...) {
   maxdur <- table[, max(duration)]
-  observed_statistics <- zip_calculations(table, zones, maxdur = maxdur, ...)
-  scan_obs <- extract_scanstatistic(observed_statistics)
-  
-  replicate_scanstats <- zip_mcsim(table, 
-                                   zones, 
-                                   n_replicates, 
-                                   maxdur = maxdur,
-                                   ...)
-  pval <- mc_pvalue(scan_obs, replicate_scanstats)
-  
-  list(data = table,
-       zones = zones,
-       n_replicates = n_replicates,
-       replicates = replicate_scanstats,
-       observed = observed_statistics,
-       mlc = extract_mlc(observed_statistics),
-       pvalue = pval)
+  scanstatistic_object(zip_calculations(table, zones, maxdur, ...), 
+                       zip_mcsim(table, zones, n_mcsim, maxdur, ...),
+                       list(table = table,
+                            zones = zones, 
+                            distribution = "zero-inflated Poisson",
+                            type = "Expectation-based"))
 }
 
 
@@ -61,7 +112,7 @@ zip_scanstatistic <- function(table, zones, n_replicates, ...) {
 #' @importFrom gamlss.dist rZIP
 #' @importFrom stats rpois
 #' @keywords internal
-generate_zip_counts <- function(table, abs_tol = 1e08) {
+generate_zip_counts <- function(table, abs_tol = 1e-08) {
   # Note: rZIP returns a numeric vector
   table[, count := 0]
   table[p < abs_tol, count := as.numeric(rpois(.N, mean))]
@@ -90,21 +141,26 @@ simulate_zip_scanstatistic <- function(table, zones, ...) {
 
 #' Monte Carlo simulation of expectation-based Poisson scan statistics.
 #' 
-#' This function generates \code{n_replicates} Poisson-distributed data sets 
+#' This function generates \code{n_mcsim} Poisson-distributed data sets 
 #' according to the parameters in the input table, and calculates the value of
 #' the scan statistic for each generated data set using the supplied 
 #' \code{zones}.
 #' @param table A \code{data.table} with columns \code{location, duration, p,
 #'    mean}.
 #' @inheritParams partition_zones
-#' @param n_replicates A positive integer; the number of replicate scan 
+#' @param n_mcsim A positive integer; the number of replicate scan 
 #'    statistics to generate.
 #' @param ... Arguments passed to \code{\link{simulate_zip_scanstatistic}}.
-#' @return A numeric vector of length \code{n_replicates}.
+#' @return A numeric vector of length \code{n_mcsim}.
 #' @importFrom magrittr %>%
 #' @keywords internal
-zip_mcsim <- function(table, zones, n_replicates = 999L, ...) {
-  replicate(n_replicates, simulate_zip_scanstatistic(table, zones, ...))
+zip_mcsim <- function(table, zones, n_mcsim = 0, ...) {
+  if (n_mcsim > 0) {
+    return(replicate(n_mcsim, simulate_poisson_scanstatistic(table, zones)))
+  } else {
+    return(numeric(0))
+  }
+  replicate(n_mcsim, simulate_zip_scanstatistic(table, zones, ...))
 }
 
 
