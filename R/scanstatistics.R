2e4005b1e7bb03ca49df8ff379460c9b9a0fbe76
Benjamin <benjak@math.su.se>
2016-09-28 08:09:43 +0200

    Updated package description to match DESCRIPTION.

--- R/scanstatistics.R
+++ R/scanstatistics.R
@@ -1,4 +1,4 @@
-#' scanstatistics: A package for spatiotemporal event detection.
+#' scanstatistics: Space-time anomaly detection using scan statistics.
 #'
 #' The scanstatistics package provides two categories of important functions:
 #' data preparation functions, and the scan statistics themselves.
@@ -6,7 +6,10 @@
 #' These functions prepare your data for use. In particular, it helps you 
 #' define the \emph{zones} which will be considered by the scan statistics.
 #' @section Scan statistics:
-#' These are the functions used for spacetime cluster detection.
+#' These are the functions used for space-time anomaly detection. Scan statistic
+#' functions for univariate space-time data have a name that begins with 
+#' \code{scan_} and functions for multivariate space-time data have a name that
+#' begins with \code{mscan_}.
 #' @docType package
 #' @name scanstatistics
 #' @import data.table
