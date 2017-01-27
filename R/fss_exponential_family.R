8c88bb145c3e1054a913556eed736270ec99a486
Benjamin Kjellson <benjak@math.su.se>
2017-01-27 16:30:06 +0100

    Functions and tests for Gaussian.

--- R/fss_exponential_family.R
+++ R/fss_exponential_family.R
@@ -26,7 +26,7 @@ poisson_qmax <- function(c, b) {
 #' @param baselines A matrix of expected counts with the same dimensions as
 #'    \code{counts}.
 #' @param scalar_priority_fun A function taking two arguments \code{c} (count)
-#'    and \code{b} (baseline) and 
+#'    and \code{b} (baseline) and returning a scalar.
 #' @return A matrix with the same dimensions as \code{counts}. The \eqn{i}th 
 #'    element in each column contains the Poisson priority function value for 
 #'    the window of time (duration) stretching from 1 to \eqn{i}, for that
@@ -71,6 +71,81 @@ poisson_score <- function(counts, baselines, priority_indices) {
   # Compute scores for corresponding element pairs in the count and baseline
   # matrices
   matrix(map2_dbl(counts, baselines, poisson_lambda), 
+         nrow(priority_indices), 
+         ncol(priority_indices))
+}
+
+# Gaussian ---------------------------------------------------------------------
+
+#' Calculates the largest value for which the Gaussian score function is zero.
+#' @param c A scalar; the observed value.
+#' @param b A scalar; the expected value.
+#' @return A scalar greater than or equal to 1.
+#' @keywords internal
+gaussian_qmax <- function(c, b) {
+  ifelse(c > b, 2 * c / b - 1, 1)
+}
+
+#' Compute Gaussian priority function values cumulatively over time.
+#' 
+#' Given a matrices with observed and expected values (baselines) for each 
+#' timepoint (row) and location/data stream (column), sum counts and baselines 
+#' cumulatively backwards in time, and compute the Gaussian priority function
+#' value for each column and subset of time.
+#' @param counts A matrix of observed counts. Rows represent timepoints, ordered
+#'    from most recent to most distant. Columns represent e.g. locations or
+#'    data streams.
+#' @param baselines A matrix of expected counts with the same dimensions as
+#'    \code{counts}.
+#' @param scalar_priority_fun A function taking two arguments \code{c} (count)
+#'    and \code{b} (baseline) and returning a scalar.
+#' @return A matrix with the same dimensions as \code{counts}. The \eqn{i}th 
+#'    element in each column contains the Poisson priority function value for 
+#'    the window of time (duration) stretching from 1 to \eqn{i}, for that
+#'    column.
+#' @importFrom purrr map2_dbl
+#' @keywords internal
+gaussian_priority <- function(counts, 
+                              baselines, 
+                              scalar_priority_fun = gaussian_qmax) {
+  agg_c <- as.vector(apply(counts, 2, cumsum))
+  agg_b <- as.vector(apply(baselines, 2, cumsum))
+  prios <- map2_dbl(agg_c, agg_b, scalar_priority_fun)
+  matrix(prios, nrow(counts), ncol(counts))
+}
+
+#' Compute the score for an individual Gaussian observation.
+#' @param c A scalar; the observed value.
+#' @param b A scalar; the expected value.
+#' @return A scalar; the score.
+#' @keywords internal
+gaussian_lambda <- function(c, b, s2) {
+  q <- c / b
+  c * b * (q - 1) / s2 + b^2 * (1 - q^2) / (2 * s2)
+}
+
+#' Compute the Gaussian score for each priority subset, for each duration.
+#' @inheritParams gaussian_priority
+#' @param priority_indices A matrix of the same size as the input. On each row
+#'    (duration), column numbers are given in order of priority.
+#' @return A matrix of the same dimension as the input matrices.
+#' @importFrom purrr pmap_dbl
+#' @keywords internal
+gaussian_score <- function(counts, baselines, variances, priority_indices) {
+  # Since the sum of Gaussian random variables is Gaussian with parameters 
+  # (expected value and variance) equal to the sum of the individual parameters, 
+  # compute cumulative sums over duration (rows) for each column. Then 
+  # re-arrange values in each row by priority, and use the same property again 
+  # by computing cumulative sums over locations/data streams (columns) for each 
+  # row.
+  counts <- sum_reorder_sum(counts, priority_indices)
+  baselines <- sum_reorder_sum(baselines, priority_indices)
+  variances <- sum_reorder_sum(variances, priority_indices)
+  args <- list(c = counts, b = baselines, s2 = variances)
+  
+  # Compute scores for corresponding element pairs in the count and baseline
+  # matrices
+  matrix(pmap_dbl(.l = args, .f = gaussian_lambda), 
          nrow(priority_indices), 
          ncol(priority_indices))
 }
--- tests/testthat/test_fss_exponential_family.R
+++ tests/testthat/test_fss_exponential_family.R
@@ -1,5 +1,7 @@
 context("Fast Subset Scan: Exponential Family Functions")
 
+# Poisson ----------------------------------------------------------------------
+
 test_that("poisson_qmax", {
   expect_equal(poisson_qmax(5, 5), 1)
   # From Wolfram Alpha: solve 10*log(x)+5*(1-x)=0
@@ -34,4 +36,36 @@ test_that("poisson_score", {
   expect_equal(actual, expected)
 })
 
+# Gaussian ---------------------------------------------------------------------
 
+test_that("gaussian_priority", {
+  B <- matrix(c(1, 3,
+                2, 4), 2, 2, byrow = TRUE)
+  C <- matrix(c(2, 3,
+                1, 5), 2, 2, byrow = TRUE)
+  expected <- matrix(c(gaussian_qmax(2, 1), 1,
+                       1,                  gaussian_qmax(8, 7)),
+                     2, 2, byrow = TRUE)
+  actual <- gaussian_priority(C, B, gaussian_qmax)
+  expect_equal(actual, expected)
+})
+
+test_that("gaussian_score", {
+  pri_mat <- matrix(c(1, 2,
+                      2, 1), 
+                    2, 2, byrow = TRUE)
+  B <- matrix(c(1, 3,
+                2, 4), 2, 2, byrow = TRUE)
+  C <- matrix(c(2, 3,
+                1, 5), 2, 2, byrow = TRUE)
+  S2 <- matrix(c(1, 2, 
+                 3, 4),
+               2, 2, byrow = TRUE)
+  expected <- matrix(c(gaussian_lambda(2, 1, 1), gaussian_lambda(2+3,1+3, 1+2),
+                       gaussian_lambda(3+5, 3+4, 2+4), gaussian_lambda(2+1+3+5,
+                                                                      1+2+3+4,
+                                                                      1+2+3+4)),
+                     2, 2, byrow = TRUE)
+  actual <- gaussian_score(C, B, S2, pri_mat)
+  expect_equal(actual, expected)
+})
