10c8ee311632eab6bf1732d3379f99d782a0b931
Benjamin Kjellson <benjak@math.su.se>
2017-04-06 14:23:42 +0200

    Update tests to new function names.

--- tests/testthat/test_scan_zip_eb.R
+++ tests/testthat/test_scan_zip_eb.R
@@ -1,26 +1,12 @@
-context("ZIP statistic tests")
+context("EB ZIP statistic tests")
 
-test_that("incomplete_loglihood_term", {
-  expect_equal(incomplete_loglihood_term(0, 3, 0.2, 2),
-               log(0.2 + 0.8 * exp(-2 * 3)))
-  expect_equal(incomplete_loglihood_term(1, 3, 0.2, 2),
-               log(0.8) + 1 * log(2 * 3) - lgamma(1 + 1) - 2 * 3)
 
-})
-
-test_that("incomplete_loglihood", {
-  expect_equal(incomplete_loglihood(c(0, 1), c(3, 3), c(0.2, 0.2), 2),
-               log(0.2 + 0.8 * exp(-2 * 3)) + 
-               log(0.8) + 1 * log(2 * 3) - lgamma(1 + 1) - 2 * 3)
-  
-})
-
-test_that("estimate_q", {
+test_that("est_zip_relrisk", {
   y1 <- c(1, 0); mu1 <- c(0.5, 2); p1 <- c(0.1, 0.2); d1 <- c(0, 0.65)
-  expect_equal(estimate_q(sum(y1), mu1, p1, d1), 1)
+  expect_equal(est_zip_relrisk(sum(y1), mu1, p1, d1), 1)
   
   y2 <- c(2, 2); mu2 <- c(1, 1); p2 <- c(0.1, 0.2); d2 <- c(0, 0)
-  expect_equal(estimate_q(sum(y2), mu2, p2, d2), 2)
+  expect_equal(est_zip_relrisk(sum(y2), mu2, p2, d2), 2)
 })
 
 test_that("score_zip", {
@@ -32,11 +18,12 @@ test_that("score_zip", {
   expect_equal(out1_actual[[2]], out1_expected[[2]])
   
   # This input holds no zeros and should give q_hat = 2
-  in2 <- list(y = c(2,2), mu = c(1, 1), p = c(0.1, 0.2))
-  out2_expected <- list(2 * (incomplete_loglihood_term(2, 1, 0.1, 2) - 
-                               incomplete_loglihood_term(2, 1, 0.1, 1)), 2) 
+  in2 <- list(y = c(2, 2), mu = c(1, 1), p = c(0.1, 0.2))
+  out2_expected <- list(2 * (zip_lpmf(2, 2, 0.1) - 
+                               zip_lpmf(2, 1, 0.1)), 
+                        2)
   out2_actual <- score_zip(in2$y, in2$mu, in2$p, 1e-3)
-  expect_equal(out2_actual[[1]], out2_expected[[1]])
+  expect_equal(as.numeric(out2_actual[[1]]), as.numeric(out2_expected[[1]]))
   expect_equal(out2_actual[[2]], out2_expected[[2]])
   
 })
@@ -53,8 +40,8 @@ test_that("calc_all_zip_eb", {
   
   actual1 <- calc_all_zip_eb(in1$counts, in1$baselines, in1$probs, 
                              in1$zones_flat - 1, in1$zone_lengths)
-  expected1_score <- c(incomplete_loglihood_term(1, 0.5, 0.1, 2) - 
-                         incomplete_loglihood_term(1, 0.5, 0.1, 1),
+  expected1_score <- c(zip_lpmf(1, 1, 0.1) - 
+                         zip_lpmf(1, 0.5, 0.1),
                        0, 0)
   expect_equal(actual1$score, expected1_score)
   expect_equal(actual1$relrisk, c(2, 1, 1))
@@ -83,37 +70,31 @@ test_that("calc_all_zip_eb", {
     1, 
     3, 
     1,  
-    4 / (3 + 2 * (1 - estimate_struc_zero(2, 0.2, 1))),
-    3 / (1 + 0.5 * (1 - estimate_struc_zero(0.5, 0.1, 1))),
-    21 / (4 + 2 * (1 - estimate_struc_zero(
-      2, 0.2, 21 / (4 + 2 * (1 - estimate_struc_zero(2, 0.2, 1)))))))
+    4 / (3 + 2 * (1 - est_zip_zero_indicator(2, 0.2, 1))),
+    3 / (1 + 0.5 * (1 - est_zip_zero_indicator(0.5, 0.1, 1))),
+    21 / (4 + 2 * (1 - est_zip_zero_indicator(
+      2, 0.2, 21 / (4 + 2 * (1 - est_zip_zero_indicator(2, 0.2, 1)))))))
   
   expected2_score <- c(
     # Duration = 1
-    incomplete_loglihood_term(1, 0.5, 0.1, expected2_relrisk[1]) - 
-      incomplete_loglihood_term(1, 0.5, 0.1, 1),
+    zip_lpmf(1, expected2_relrisk[1] * 0.5, 0.1) - 
+      zip_lpmf(1, 0.5, 0.1),
      0, 0,
     # Duration = 2
-     incomplete_loglihood_term(3, 1, 0.1, expected2_relrisk[4]) - 
-       incomplete_loglihood_term(3, 1, 0.1, 1),
+     zip_lpmf(3, expected2_relrisk[4] * 1, 0.1) - 
+       zip_lpmf(3, 1, 0.1),
      0,
-     incomplete_loglihood(c(1, 2, 0, 1),
-                          c(0.5, 0.5, 2, 2),
-                          c(0.1, 0.1, 0.2, 0.2),
-                          expected2_relrisk[6]) - 
-       incomplete_loglihood(c(1, 2, 0, 1),
-                            c(0.5, 0.5, 2, 2),
-                            c(0.1, 0.1, 0.2, 0.2),
-                            1),
+     zip_loglihood(c(1, 2, 0, 1), c(0.5, 0.5, 2, 2), c(0.1, 0.1, 0.2, 0.2),
+                   expected2_relrisk[6]) - 
+       zip_loglihood(c(1, 2, 0, 1), c(0.5, 0.5, 2, 2), c(0.1, 0.1, 0.2, 0.2), 
+                     1),
     # Duration = 3
     # zone = 1
-     incomplete_loglihood(
-       c(1, 2, 0), rep(0.5, 3), rep(0.1, 3), expected2_relrisk[7]) - 
-       incomplete_loglihood(c(1, 2, 0), rep(0.5, 3), rep(0.1, 3), 1),
+     zip_loglihood(c(1, 2, 0), rep(0.5, 3), rep(0.1, 3), expected2_relrisk[7]) - 
+       zip_loglihood(c(1, 2, 0), rep(0.5, 3), rep(0.1, 3), 1),
     # zone = 2
-     incomplete_loglihood(c(0, 1, 20), rep(2, 3), rep(0.2, 3),
-                          expected2_relrisk[8]) - 
-       incomplete_loglihood(c(0, 1, 20), rep(2, 3), rep(0.2, 3), 1))
+     zip_loglihood(c(0, 1, 20), rep(2, 3), rep(0.2, 3), expected2_relrisk[8]) - 
+       zip_loglihood(c(0, 1, 20), rep(2, 3), rep(0.2, 3), 1))
   
   expect_equal(actual2$score[-9], expected2_score)
   expect_equal(actual2$relrisk[-9], expected2_relrisk)
@@ -131,8 +112,8 @@ test_that("calc_one_zip_eb", {
   
   actual1 <- calc_one_zip_eb(in1$counts, in1$baselines, in1$probs, 
                              in1$zones_flat - 1, in1$zone_lengths)
-  expected1_score <- incomplete_loglihood_term(1, 0.5, 0.1, 2) - 
-                       incomplete_loglihood_term(1, 0.5, 0.1, 1)
+  expected1_score <- zip_lpmf(1, 1, 0.1) - 
+                       zip_lpmf(1, 0.5, 0.1)
   expect_equal(actual1$score, expected1_score)
   expect_equal(actual1$relrisk, 2)
   expect_equal(actual1$zone, 1)
@@ -155,12 +136,11 @@ test_that("calc_one_zip_eb", {
   actual2 <- calc_one_zip_eb(in2$counts, in2$baselines, in2$probs, 
                              in2$zones_flat - 1, in2$zone_lengths)
   
-  expected2_relrisk <- 21 / (4 + 2 * (1 - estimate_struc_zero(
-    2, 0.2, 21 / (4 + 2 * (1 - estimate_struc_zero(2, 0.2, 1))))))
+  expected2_relrisk <- 21 / (4 + 2 * (1 - est_zip_zero_indicator(
+    2, 0.2, 21 / (4 + 2 * (1 - est_zip_zero_indicator(2, 0.2, 1))))))
   expected2_score <- 
-    incomplete_loglihood(c(0, 1, 20), rep(2, 3), rep(0.2, 3), 
-                         expected2_relrisk) - 
-    incomplete_loglihood(c(0, 1, 20), rep(2, 3), rep(0.2, 3), 1)
+    zip_loglihood(c(0, 1, 20), rep(2, 3), rep(0.2, 3), expected2_relrisk) - 
+    zip_loglihood(c(0, 1, 20), rep(2, 3), rep(0.2, 3), 1)
   expect_equal(actual2$score, expected2_score)
   expect_equal(actual2$relrisk, expected2_relrisk)
   expect_equal(actual2$zone, 2)
