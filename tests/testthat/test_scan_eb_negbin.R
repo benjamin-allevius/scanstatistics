d4f4e414e08cd82a2bfce17e23629b3eb9c7ebc0
Benjamin Allévius <benjak@math.su.se>
2017-07-01 16:09:34 +0200

    Tests up.

---
+++ tests/testthat/test_scan_eb_negbin.R
@@ -0,0 +1,218 @@
+context("EB negbin statistic tests")
+
+# Helper functions -----------------------------------------------------------
+sc_hot <- function(y, m, w) {
+  num <- sum((y - m) / w)
+  den <- sum(m / w)
+  return(num / den)
+}
+
+sc_inc <- function(y, m, w, d) {
+  num <- vapply(1:ncol(y),
+                function(i) sum( (y[ , i] - m[ , i]) * (d:1) / w[ , i]),
+                numeric(1))
+  den <- vapply(1:ncol(y),
+                function(i) sum( m[ , i] * (d:1)^2 / w[ , i]),
+                numeric(1))
+  return(sum(num) / sum(den))
+}
+
+test_that("scan_eb_negbin: 1 timepoint, hotspot", {
+  
+  # Single timepoint -----------------------------------------------------------
+  in1 <- list(
+    counts = matrix(c(1, 0), nrow = 1),
+    baselines = matrix(c(0.5, 2), nrow = 1),
+    overdisp = matrix(c(1.5, 1.5), nrow = 1),
+    zones = list(1L, 2L, 1:2))
+  in1$zones_flat =  unlist(in1$zones)
+  in1$zone_lengths = unlist(lapply(in1$zones, length))
+  
+  actual1 <- scan_eb_negbin_cpp(in1$counts, 
+                                in1$baselines, 
+                                in1$overdisp,
+                                 in1$zones_flat - 1, 
+                                 in1$zone_lengths,
+                                 ncol(in1$counts),
+                                 length(in1$zones),
+                                 nrow(in1$counts),
+                                 store_everything = TRUE,
+                                score_type = TRUE) # hotspot
+  actual1b <- scan_eb_negbin_cpp(in1$counts, 
+                                in1$baselines, 
+                                in1$overdisp,
+                                in1$zones_flat - 1, 
+                                in1$zone_lengths,
+                                ncol(in1$counts),
+                                length(in1$zones),
+                                nrow(in1$counts),
+                                store_everything = FALSE,
+                                score_type = TRUE) # hotspot
+
+  expected1_score <- c(
+    # Duration = 1
+    sc_hot(in1$counts[1, 1],   in1$baselines[1, 1],   in1$overdisp[1, 1]),
+    sc_hot(in1$counts[1, 2],   in1$baselines[1, 2],   in1$overdisp[1, 2]),
+    sc_hot(in1$counts[1, 1:2], in1$baselines[1, 1:2], in1$overdisp[1, 1:2]))
+  expect_equal(actual1$score, expected1_score)
+  expect_equal(c(actual1[which.max(actual1$score), ]), c(actual1b))
+})
+  
+  
+test_that("scan_eb_negbin: 3 timepoints, hotspot", {
+  
+  # 3 timepoints ---------------------------------------------------------------
+  in2 <- list(
+    counts = matrix(c(1, 0,
+                      2, 1,
+                      0, 20), nrow = 3, byrow = TRUE),
+    baselines = matrix(c(0.5, 2,
+                         0.5, 2,
+                         0.5, 2), nrow = 3, byrow = TRUE),
+    overdisp = matrix(c(1.5, 2,
+                        1.5, 2,
+                        1.5, 2), nrow = 3, byrow = TRUE),
+    zones = list(1L, 2L, 1:2))
+  in2$zones_flat =  unlist(in2$zones)
+  in2$zone_lengths = unlist(lapply(in2$zones, length))
+  
+  actual2 <- scan_eb_negbin_cpp(in2$counts, 
+                                in2$baselines, 
+                                in2$overdisp,
+                                in2$zones_flat - 1, 
+                                in2$zone_lengths,
+                                ncol(in2$counts),
+                                length(in2$zones),
+                                nrow(in2$counts),
+                                store_everything = TRUE,
+                                score_type = TRUE) # hotspot
+  
+  actual2b <- scan_eb_negbin_cpp(in2$counts, 
+                                in2$baselines, 
+                                in2$overdisp,
+                                in2$zones_flat - 1, 
+                                in2$zone_lengths,
+                                ncol(in2$counts),
+                                length(in2$zones),
+                                nrow(in2$counts),
+                                store_everything = FALSE,
+                                score_type = TRUE) # hotspot
+  
+  expected2_score <- c(
+    # Duration = 1
+    sc_hot(in2$counts[1, 1],   in2$baselines[1, 1],   in2$overdisp[1, 1]),
+    sc_hot(in2$counts[1, 2],   in2$baselines[1, 2],   in2$overdisp[1, 2]),
+    sc_hot(in2$counts[1, 1:2], in2$baselines[1, 1:2], in2$overdisp[1, 1:2]),
+    # Duration = 2
+    sc_hot(in2$counts[1:2, 1],   in2$baselines[1:2, 1],   in2$overdisp[1:2, 1]),
+    sc_hot(in2$counts[1:2, 2],   in2$baselines[1:2, 2],   in2$overdisp[1:2, 2]),
+    sc_hot(in2$counts[1:2, 1:2], in2$baselines[1:2, 1:2], in2$overdisp[1:2, 1:2]),
+    # Duration = 3
+    sc_hot(in2$counts[1:3, 1],   in2$baselines[1:3, 1],   in2$overdisp[1:3, 1]),
+    sc_hot(in2$counts[1:3, 2],   in2$baselines[1:3, 2],   in2$overdisp[1:3, 2]),
+    sc_hot(in2$counts[1:3, 1:2], in2$baselines[1:3, 1:2], in2$overdisp[1:3, 1:2]))
+
+  expect_equal(actual2$score, expected2_score)
+  expect_equal(c(actual2[which.max(actual2$score), ]), c(actual2b))
+})
+
+
+test_that("scan_eb_negbin: 1 timepoint, emerging", {
+  
+  # Single timepoint -----------------------------------------------------------
+  in1 <- list(
+    counts = matrix(c(1, 0), nrow = 1),
+    baselines = matrix(c(0.5, 2), nrow = 1),
+    overdisp = matrix(c(1.5, 1.5), nrow = 1),
+    zones = list(1L, 2L, 1:2))
+  in1$zones_flat =  unlist(in1$zones)
+  in1$zone_lengths = unlist(lapply(in1$zones, length))
+  
+  actual1 <- scan_eb_negbin_cpp(in1$counts, 
+                                in1$baselines, 
+                                in1$overdisp,
+                                in1$zones_flat - 1, 
+                                in1$zone_lengths,
+                                ncol(in1$counts),
+                                length(in1$zones),
+                                nrow(in1$counts),
+                                store_everything = TRUE,
+                                score_type = FALSE) # emerging
+  actual1b <- scan_eb_negbin_cpp(in1$counts, 
+                                 in1$baselines, 
+                                 in1$overdisp,
+                                 in1$zones_flat - 1, 
+                                 in1$zone_lengths,
+                                 ncol(in1$counts),
+                                 length(in1$zones),
+                                 nrow(in1$counts),
+                                 store_everything = FALSE,
+                                 score_type = FALSE) # emerging
+  
+  expected1_score <- c(
+    # Duration = 1
+    sc_inc(in1$counts[1, 1, drop=F],   in1$baselines[1, 1, drop=F],   in1$overdisp[1, 1, drop=F], 1),
+    sc_inc(in1$counts[1, 2, drop=F],   in1$baselines[1, 2, drop=F],   in1$overdisp[1, 2, drop=F], 1),
+    sc_inc(in1$counts[1, 1:2, drop=F], in1$baselines[1, 1:2, drop=F], in1$overdisp[1, 1:2, drop=F], 1))
+  
+  expect_equal(actual1$score, expected1_score)
+  expect_equal(c(actual1[which.max(actual1$score), ]), c(actual1b))
+})
+
+
+test_that("scan_eb_negbin: 3 timepoints, emerging", {
+  
+  # 3 timepoints ---------------------------------------------------------------
+  in2 <- list(
+    counts = matrix(c(1, 0,
+                      2, 1,
+                      0, 20), nrow = 3, byrow = TRUE),
+    baselines = matrix(c(0.5, 2,
+                         0.5, 2,
+                         0.5, 2), nrow = 3, byrow = TRUE),
+    overdisp = matrix(c(1.5, 2,
+                        1.5, 2,
+                        1.5, 2), nrow = 3, byrow = TRUE),
+    zones = list(1L, 2L, 1:2))
+  in2$zones_flat =  unlist(in2$zones)
+  in2$zone_lengths = unlist(lapply(in2$zones, length))
+  
+  actual2 <- scan_eb_negbin_cpp(in2$counts, 
+                                in2$baselines, 
+                                in2$overdisp,
+                                in2$zones_flat - 1, 
+                                in2$zone_lengths,
+                                ncol(in2$counts),
+                                length(in2$zones),
+                                nrow(in2$counts),
+                                store_everything = TRUE,
+                                score_type = FALSE) # hotspot
+  
+  actual2b <- scan_eb_negbin_cpp(in2$counts, 
+                                 in2$baselines, 
+                                 in2$overdisp,
+                                 in2$zones_flat - 1, 
+                                 in2$zone_lengths,
+                                 ncol(in2$counts),
+                                 length(in2$zones),
+                                 nrow(in2$counts),
+                                 store_everything = FALSE,
+                                 score_type = FALSE) # hotspot
+  
+  expected2_score <- c(
+    # Duration = 1
+    sc_inc(in2$counts[1, 1, drop=F],   in2$baselines[1, 1, drop=F],   in2$overdisp[1, 1, drop=F], 1),
+    sc_inc(in2$counts[1, 2, drop=F],   in2$baselines[1, 2, drop=F],   in2$overdisp[1, 2, drop=F], 1),
+    sc_inc(in2$counts[1, 1:2, drop=F], in2$baselines[1, 1:2, drop=F], in2$overdisp[1, 1:2, drop=F], 1),
+    # Duration = 2
+    sc_inc(in2$counts[1:2, 1, drop=F],   in2$baselines[1:2, 1, drop=F],   in2$overdisp[1:2, 1, drop=F], 2),
+    sc_inc(in2$counts[1:2, 2, drop=F],   in2$baselines[1:2, 2, drop=F],   in2$overdisp[1:2, 2, drop=F], 2),
+    sc_inc(in2$counts[1:2, 1:2, drop=F], in2$baselines[1:2, 1:2, drop=F], in2$overdisp[1:2, 1:2], 2),
+    # Duration = 3
+    sc_inc(in2$counts[1:3, 1, drop=F],   in2$baselines[1:3, 1, drop=F],   in2$overdisp[1:3, 1, drop=F], 3),
+    sc_inc(in2$counts[1:3, 2, drop=F],   in2$baselines[1:3, 2, drop=F],   in2$overdisp[1:3, 2, drop=F], 3),
+    sc_inc(in2$counts[1:3, 1:2, drop=F], in2$baselines[1:3, 1:2, drop=F], in2$overdisp[1:3, 1:2, drop=F], 3))
+  
+  expect_equal(actual2$score, expected2_score)
+  expect_equal(c(actual2[which.max(actual2$score), ]), c(actual2b))
+})
--- tests/testthat/test_scan_pb_poisson.R
+++ tests/testthat/test_scan_pb_poisson.R
@@ -1,56 +1,95 @@
-# context("PB Poisson statistic tests")
-# 
-# test_that("scan_pb_poisson_cpp", {
-#   # Single timepoint
-#   in1 <- list(
-#     counts = matrix(c(1, 0), nrow = 1),
-#     baselines = matrix(c(0.5, 0.5), nrow = 1),
-#     zones = list(1L, 2L, 1:2))
-#   in1$N = sum(in1$counts)
-#   in1$zones_flat =  unlist(in1$zones)
-#   in1$zone_lengths = unlist(lapply(in1$zones, length))
-#   
-#   actual1 <- scan_pb_poisson_cpp(
-#     in1$counts, in1$baselines, in1$zones_flat - 1, in1$zone_lengths, in1$N)
-#   expected1_score <- c(log(1 / 0.5), 0, 0)
-#   expect_equal(actual1$score, expected1_score)
-#   expect_equal(actual1$relrisk_in, c(2, 0, 1))
-#   expect_equal(actual1$relrisk_out, c(0, 2, 1))
-#   
-#   # 3 timepoints
-#   in2 <- list(
-#     counts = matrix(c(1, 0,
-#                       2, 1,
-#                       0, 20), nrow = 3, byrow = TRUE),
-#     baselines = matrix(c(2, 1.0,
-#                          4, 5,
-#                          6, 6), nrow = 3, byrow = TRUE),
-#     zones = list(1L, 2L, 1:2))
-#   in2$zones_flat =  unlist(in2$zones)
-#   in2$zone_lengths = unlist(lapply(in2$zones, length))
-#   in2$N <- sum(in2$counts)
-#   
-#   actual2 <- scan_pb_poisson_cpp(in2$counts,
-#                                  apply(in2$baselines, 2, cumsum),
-#                                  in2$zones_flat - 1, in2$zone_lengths,
-#                                  in2$N)
-#   
-#   expected2_relrisk_in <- c(1/2, 0, 1/3,
-#                             1/2, 1/6, 4/12,
-#                             3 / 12, 21 / 12, 1)
-#   expected2_relrisk_out <- c(23 / 22, 24 / 23, 23 / 21,
-#                              21 / 18, 23 / 18, 20 / 12,
-#                              21 / 12, 3 / 12, 1)
-#   
-#   f <- function(c, b) {
-#     term2 <- ifelse(in2$N > b, (in2$N - c) * log((in2$N - c) / (in2$N - b)), 0)
-#     ifelse(c > b, c * log(c/b) + term2, 0)
-#   }
-#   expected2_score <- c(f(1, 2), f(0, 1), f(1, 3),
-#                        f(3, 6), f(1, 6), f(4, 12),
-#                        f(3, 12), f(21, 12), f(24, 24))
-#   
-#   expect_equal(actual2$score, expected2_score)
-#   expect_equal(actual2$relrisk_in, expected2_relrisk_in)
-#   expect_equal(actual2$relrisk_out, expected2_relrisk_out)
-# })
+context("PB Poisson statistic tests")
+
+test_that("scan_pb_poisson_cpp", {
+  # Single timepoint
+  in1 <- list(
+    counts = matrix(c(1, 0), nrow = 1),
+    baselines = matrix(c(0.5, 0.5), nrow = 1),
+    zones = list(1L, 2L, 1:2))
+  in1$N = sum(in1$counts)
+  in1$zones_flat =  unlist(in1$zones)
+  in1$zone_lengths = unlist(lapply(in1$zones, length))
+  
+  agc <- matrix(apply(in1$counts, 2, cumsum), nrow = 1)
+  agb <- matrix(apply(in1$baselines, 2, cumsum), nrow = 1)
+
+  actual1 <- scan_pb_poisson_cpp(agc,
+                                 agb,
+                                 in1$N,
+                                 in1$zones_flat - 1, 
+                                 in1$zone_lengths, 
+                                 ncol(in1$counts),
+                                 length(in1$zones),
+                                 nrow(in1$counts),
+                                 store_everything = TRUE)
+  actual1b <- scan_pb_poisson_cpp(agc,
+                                 agb,
+                                 in1$N,
+                                 in1$zones_flat - 1, 
+                                 in1$zone_lengths, 
+                                 ncol(in1$counts),
+                                 length(in1$zones),
+                                 nrow(in1$counts),
+                                 store_everything = FALSE)
+  expected1_score <- c(log(1 / 0.5), 0, 0)
+  expect_equal(actual1$score, expected1_score)
+  expect_equal(actual1$relrisk_in, c(2, 0, 1))
+  expect_equal(actual1$relrisk_out, c(0, 2, 1))
+  expect_equal(c(actual1[which.max(actual1$score), ]), c(actual1b))
+
+  # 3 timepoints
+  in2 <- list(
+    counts = matrix(c(1, 0,
+                      2, 1,
+                      0, 20), nrow = 3, byrow = TRUE),
+    baselines = matrix(c(2, 1.0,
+                         4, 5,
+                         6, 6), nrow = 3, byrow = TRUE),
+    zones = list(1L, 2L, 1:2))
+  in2$zones_flat =  unlist(in2$zones)
+  in2$zone_lengths = unlist(lapply(in2$zones, length))
+  in2$N <- sum(in2$counts)
+  
+  agc2 <- apply(in2$counts, 2, cumsum)
+  agb2 <- apply(in2$baselines, 2, cumsum)
+  
+
+  actual2 <- scan_pb_poisson_cpp(agc2,
+                                 agb2,
+                                 in2$N,
+                                 in2$zones_flat - 1, 
+                                 in2$zone_lengths, 
+                                 ncol(in2$counts),
+                                 length(in2$zones),
+                                 nrow(in2$counts),
+                                 store_everything = TRUE)
+  actual2b <- scan_pb_poisson_cpp(agc2,
+                                 agb2,
+                                 in2$N,
+                                 in2$zones_flat - 1, 
+                                 in2$zone_lengths, 
+                                 ncol(in2$counts),
+                                 length(in2$zones),
+                                 nrow(in2$counts),
+                                 store_everything = FALSE)
+
+  expected2_relrisk_in <- c(1/2, 0, 1/3,
+                            1/2, 1/6, 4/12,
+                            3 / 12, 21 / 12, 1)
+  expected2_relrisk_out <- c(23 / 22, 24 / 23, 23 / 21,
+                             21 / 18, 23 / 18, 20 / 12,
+                             21 / 12, 3 / 12, 1)
+
+  f <- function(c, b) {
+    term2 <- ifelse(in2$N > b, (in2$N - c) * log((in2$N - c) / (in2$N - b)), 0)
+    ifelse(c > b, c * log(c/b) + term2, 0)
+  }
+  expected2_score <- c(f(1, 2), f(0, 1), f(1, 3),
+                       f(3, 6), f(1, 6), f(4, 12),
+                       f(3, 12), f(21, 12), f(24, 24))
+
+  expect_equal(actual2$score, expected2_score)
+  expect_equal(actual2$relrisk_in, expected2_relrisk_in)
+  expect_equal(actual2$relrisk_out, expected2_relrisk_out)
+  expect_equal(c(actual2[which.max(actual2$score), ]), c(actual2b))
+})
