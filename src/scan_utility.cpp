ebd6f53885d702fc9ffa01133dff24d4eb78ad1b
Benjamin Allévius <benjamin.allevius@gmail.com>
2017-08-07 20:16:39 +0200

    Functions for permuting matrix entries.

--- src/scan_utility.cpp
+++ src/scan_utility.cpp
@@ -8,14 +8,71 @@ std::vector<int> get_zero_indices(const arma::uvec& v) {
   return zero_idx;
 }
 
-Rcpp::NumericVector armaVec2rcppVec(const arma::vec& v) {
+
+// Rcpp vector to armadillo vectors --------------------------------------------
+
+arma::vec NumericVector2vec(const Rcpp::NumericVector& v) {
+  arma::vec x(v.size());
+  for (int i = 0; i < v.size(); ++i) x.at(i) = v.at(i);
+  return x;
+}
+
+arma::uvec IntegerVector2uvec(const Rcpp::IntegerVector& v) {
+  arma::uvec x(v.size());
+  for (int i = 0; i < v.size(); ++i) x.at(i) = v.at(i);
+  return x;
+}
+
+// armadillo vectors to Rcpp vectors -------------------------------------------
+
+Rcpp::NumericVector vec2NumericVector(const arma::vec& v) {
   Rcpp::NumericVector x(v.size());
   for (int i = 0; i < v.size(); ++i) x.at(i) = v.at(i);
   return x;
 }
 
-arma::uvec rcppIVec2armaVec(const Rcpp::IntegerVector& v) {
-  arma::uvec x(v.size());
+Rcpp::IntegerVector uvec2IntegerVector(const arma::uvec& v) {
+  Rcpp::IntegerVector x(v.size());
   for (int i = 0; i < v.size(); ++i) x.at(i) = v.at(i);
   return x;
 }
+
+//
+
+arma::umat expand_matrix(const arma::umat& A) {
+  arma::umat res(static_cast<arma::uword>(arma::accu(A)), 2);
+  arma::uword index = 0;
+  for (arma::uword j = 0; j < A.n_cols; ++j) {
+    for (arma::uword i = 0; i < A.n_rows; ++i) {
+      for (arma::uword k = 0; k < A(i, j); ++k) {
+        res(index, 0) = i;
+        res(index, 1) = j;
+        ++index;
+      }
+    }
+  }
+  return res;
+}
+
+arma::umat contract_matrix(const arma::umat& A, 
+                           arma::uword nr, arma::uword nc) {
+  arma::umat out(nr, nc, arma::fill::zeros);
+  for (arma::uword i = 0; i < A.n_rows; ++i) {
+    ++out(A(i, 0), A(i, 1)); 
+  }
+  return out;
+}
+
+// Permute column 0 using Fisher-Yates algorithm
+arma::uvec shuffle_time_counts(const arma::uvec& v) {
+  arma::uvec res(v);
+  arma::uword k = 0;
+  for (arma::uword i = res.n_elem - 1; k < res.n_elem; --i, ++k) {
+    arma::uword j = static_cast<arma::uword>(R::runif(0, static_cast<double>(i)));
+    arma::uword tmp = res.at(i);
+    res.at(i) = res.at(j);
+    res.at(j) = tmp;
+  }
+  return res;
+}
+
--- src/scan_utility.h
+++ src/scan_utility.h
@@ -15,15 +15,36 @@
 // [[Rcpp::export]]
 std::vector<int> get_zero_indices(const arma::uvec& v);
 
-// Convert an Rcpp NumericVector to an Armadillo vec.
-arma::vec RcppVec_to_armaVec(const Rcpp::NumericVector& v);
+// Rcpp vector to armadillo vectors --------------------------------------------
 
-// Convert an Armadillo vec to an Rcpp NumericVector
-Rcpp::NumericVector armaVec2rcppVec(const arma::vec& v);
+arma::vec NumericVector2vec(const Rcpp::NumericVector& v);
 
-// Convert an Rcpp IntegerVector to an Armadillo uvec.
-arma::uvec rcppIVec2armaVec(const Rcpp::IntegerVector& v);
+arma::uvec IntegerVector2uvec(const Rcpp::IntegerVector& v);
+
+// armadillo vectors to Rcpp vectors -------------------------------------------
+
+// Convert an armadillo vec to an Rcpp NumericVector
+Rcpp::NumericVector vec2NumericVector(const arma::vec& v);
+
+// Convert an armadillo vec to an Rcpp NumericVector
+Rcpp::IntegerVector uvec2IntegerVector(const arma::uvec& v);
+
 
 // Comment: could not make template version of above 2 functions to work
+
+// Functions for permuting the counts in an integer matrix while preserving the
+// row and column marginal sums.
+// integer matrix --> expand_matrix --> permute one column --> contract matrix
+
+// [[Rcpp::export]]
+arma::umat expand_matrix(const arma::umat& A);
+
+// [[Rcpp::export]]
+arma::umat contract_matrix(const arma::umat& A, arma::uword nr, arma::uword nc);
+
+// Permute using Fisher-Yates algorithm
+// [[Rcpp::export]]
+arma::uvec shuffle_time_counts(const arma::uvec& v);
+
 
 #endif
