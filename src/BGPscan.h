9d264520340ed0fba3382c7ab1084aff29f45747
Benjamin Allévius <benjak@math.su.se>
2017-09-04 12:49:53 +0200

    Calculate on log scale also.

--- src/BGPscan.h
+++ src/BGPscan.h
@@ -19,8 +19,10 @@ public:
 
   Rcpp::DataFrame get_scan() override;
   Rcpp::List get_results();
+  Rcpp::List get_log_results();
 
 private:
+  arma::vec m_log_scores;
   arma::mat m_baselines;
   arma::mat m_baselines_orig;
   arma::uword m_total_count;
@@ -98,6 +100,8 @@ inline BGPscan::BGPscan(const arma::umat& counts,
 
   m_counts = arma::cumsum(counts);
   m_baselines = arma::cumsum(baselines);
+  
+  m_log_scores.set_size(m_out_length);
 
   m_log_window_prior = m_log_outbreak_prior
                      - (std::log(m_num_zones) + std::log(m_max_dur));
@@ -140,16 +144,18 @@ inline void BGPscan::post_process() {
   
   // Calculate the marginal probability of the data, P(D)
   m_log_data_prob = log_sum_exp(null_log_prob, 
-                                m_log_window_prior + log_sum_exp(m_scores));
+                                m_log_window_prior + log_sum_exp(m_log_scores));
   
   // Normalize the posterior probabilities
   m_log_null_posterior = null_log_prob - m_log_data_prob;
   
   for (arma::uword i = 0; i < m_scores.n_elem; ++i) {
-    m_scores.at(i) = std::exp(  m_scores.at(i) 
-                              + m_log_window_prior 
-                              - m_log_data_prob);
+    m_log_scores.at(i) = m_log_scores.at(i) 
+                       + m_log_window_prior 
+                       - m_log_data_prob;
   }
+  m_scores = arma::exp(m_log_scores);
+  
   m_log_outbreak_posterior = std::log(1.0 - std::exp(m_log_null_posterior));
   
   // 
@@ -180,8 +186,8 @@ inline double BGPscan::log_prob(const arma::uword C, const double B,
 // Storage functions -----------------------------------------------------------
 
 inline void BGPscan::store_all(arma::uword storage_index, double score,
-                                 arma::uword zone_nr, arma::uword duration) {
-  m_scores[storage_index]       = score;
+                               arma::uword zone_nr, arma::uword duration) {
+  m_log_scores[storage_index]   = score;
   m_zone_numbers[storage_index] = zone_nr;
   m_durations[storage_index]    = duration;
 }
@@ -190,21 +196,38 @@ inline void BGPscan::store_all(arma::uword storage_index, double score,
 
 inline Rcpp::DataFrame BGPscan::get_scan() {
   return Rcpp::DataFrame::create(
-    Rcpp::Named("zone")          = m_zone_numbers,
-    Rcpp::Named("duration")      = m_durations,
+    Rcpp::Named("zone")      = m_zone_numbers,
+    Rcpp::Named("duration")  = m_durations,
     Rcpp::Named("posterior") = m_scores);
 }
 
 inline Rcpp::List BGPscan::get_results() {
   return Rcpp::List::create(
-    Rcpp::Named("null_prior") = std::exp(m_log_null_prior),
-    Rcpp::Named("null_posterior") = std::exp(m_log_null_posterior),
-    Rcpp::Named("outbreak_prior") = std::exp(m_log_outbreak_prior),
-    Rcpp::Named("outbreak_posterior") = std::exp(m_log_outbreak_posterior),
-    Rcpp::Named("window_prior") = std::exp(m_log_window_prior),
-    Rcpp::Named("window_posteriors") = get_scan(),
+    Rcpp::Named("null_prior")            = std::exp(m_log_null_prior),
+    Rcpp::Named("null_posterior")        = std::exp(m_log_null_posterior),
+    Rcpp::Named("outbreak_prior")        = std::exp(m_log_outbreak_prior),
+    Rcpp::Named("outbreak_posterior")    = std::exp(m_log_outbreak_posterior),
+    Rcpp::Named("window_prior")          = std::exp(m_log_window_prior),
+    Rcpp::Named("window_posteriors")     = get_scan(),
     Rcpp::Named("space_time_posteriors") = m_spacetime_posteriors,
-    Rcpp::Named("location_posteriors") = m_location_posteriors
+    Rcpp::Named("location_posteriors")   = m_location_posteriors,
+    Rcpp::Named("marginal_data_prob")    = exp(m_log_data_prob)
+  );
+}
+
+inline Rcpp::List BGPscan::get_log_results() {
+  return Rcpp::List::create(
+    Rcpp::Named("null_prior")            = m_log_null_prior,
+    Rcpp::Named("null_posterior")        = m_log_null_posterior,
+    Rcpp::Named("outbreak_prior")        = m_log_outbreak_prior,
+    Rcpp::Named("outbreak_posterior")    = m_log_outbreak_posterior,
+    Rcpp::Named("window_prior")          = m_log_window_prior,
+    Rcpp::Named("space_time_posteriors") = m_spacetime_posteriors,
+    Rcpp::Named("location_posteriors")   = m_location_posteriors,
+    Rcpp::Named("marginal_data_prob")    = m_log_data_prob,
+    Rcpp::Named("window_zone")           = m_zone_numbers,
+    Rcpp::Named("window_duration")       = m_durations,
+    Rcpp::Named("window_log_posterior")  = m_log_scores
   );
 }
 
